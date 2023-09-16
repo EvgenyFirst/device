1be246fa6be3c7b3002f3ad190c30fa4d9005f07
EvgenyFirst <chilicklic@mail.ru>
2023-09-16 16:33:36 +0600

    page development 23

--- js/slider-price.js
+++ js/slider-price.js
@@ -2,62 +2,64 @@
 
 /* Здесь код слайдера с услугами компании на главной странице */
 
-var changeOneButton = document.querySelector(".main__card-slide-one");
-var changeTwoButton = document.querySelector(".main__card-slide-two");
-var changeThreeButton = document.querySelector(".main__card-slide-three");
+var changePriceOneButton = document.querySelector(".main__card-slide-one");
+var changePriceTwoButton = document.querySelector(".main__card-slide-two");
+var changePriceThreeButton = document.querySelector(".main__card-slide-three");
 
-var changeOneButtonAdd = document.querySelector(".main__card-button-special-one");
-var changeTwoButtonAdd = document.querySelector(".main__card-button-special-two");
-var changeThreeButtonAdd = document.querySelector(".main__card-button-special-three");
+var changePriceOneButtonAdd = document.querySelector(".main__card-button-special-one");
+var changePriceTwoButtonAdd = document.querySelector(".main__card-button-special-two");
+var changePriceThreeButtonAdd = document.querySelector(".main__card-button-special-three");
 
-var changeOneButtonAddOne = document.querySelector(".main__card-container-button-special-p-one");
-var changeTwoButtonAddTwo = document.querySelector(".main__card-button-special-two");
-var changeThreeButtonAddThree = document.querySelector(".main__card-container-button-special-p-three");
+var changePriceOneButtonAddOne = document.querySelector(".main__card-container-button-special-p-one");
+var changePriceTwoButtonAddTwo = document.querySelector(".main__card-container-button-special-p-two");
+var changePriceThreeButtonAddThree = document.querySelector(".main__card-container-button-special-p-three");
 
-var changeSlideOne = document.querySelector(".main__card-button-one");
-var changeSlideTwo = document.querySelector(".main__card-button-two");
-var changeSlideThree = document.querySelector(".main__card-button-three");
+var changePriceSlideOne = document.querySelector(".main__card-button-one");
+var changePriceSlideTwo = document.querySelector(".main__card-button-two");
+var changePriceSlideThree = document.querySelector(".main__card-button-three");
 
 // Открывает слайд 1 start
-changeSlideOne.addEventListener("click", function () {
-  changeOneButton.classList.remove("main__card-hidden");
-  changeTwoButton.classList.add("main__card-hidden");
-  changeThreeButton.classList.add("main__card-hidden");
+changePriceSlideOne.addEventListener("click", function () {
+  changePriceOneButton.classList.remove("main__card-hidden");
+  changePriceTwoButton.classList.add("main__card-hidden");
+  changePriceThreeButton.classList.add("main__card-hidden");
 
-  changeSlideOne.classList.add("main__card-container-button-active");
-  changeSlideTwo.classList.remove("main__card-container-button-active");
-  changeSlideThree.classList.remove("main__card-container-button-active");
+  changePriceSlideOne.classList.add("main__card-container-button-active");
+  changePriceSlideTwo.classList.remove("main__card-container-button-active");
+  changePriceSlideThree.classList.remove("main__card-container-button-active");
 });
 // Открывает слайд 1 end
 
 // Открывает слайд 2 start
-changeSlideTwo.addEventListener("click", function () {
-  changeOneButton.classList.add("main__card-hidden");
-  changeTwoButton.classList.remove("main__card-hidden");
-  changeThreeButton.classList.add("main__card-hidden");
+changePriceSlideTwo.addEventListener("click", function () {
+  changePriceOneButton.classList.add("main__card-hidden");
+  changePriceTwoButton.classList.remove("main__card-hidden");
+  changePriceThreeButton.classList.add("main__card-hidden");
 
-  changeTwoButtonAdd.classList.add("main__card-container-button-special-active");
-  changeOneButtonAdd.classList.remove("main__card-container-button-special-active");
+  changePriceTwoButtonAdd.classList.remove("main__card-container-button-special");
+  changePriceTwoButtonAddTwo.classList.add("main__card-container-button-special-p-active");
+  changePriceOneButtonAdd.classList.remove("main__card-container-button-special-active");
+  changePriceOneButtonAddOne.classList.remove("main__card-container-button-special-p-active");
 
-  changeSlideOne.classList.remove("main__card-container-button-active");
-  changeSlideTwo.classList.add("main__card-container-button-active");
-  changeSlideThree.classList.remove("main__card-container-button-active");
+  changePriceSlideOne.classList.remove("main__card-container-button-active");
+  changePriceSlideTwo.classList.add("main__card-container-button-active");
+  changePriceSlideThree.classList.remove("main__card-container-button-active");
 });
 // Открывает слайд 2 end
 
 // Открывает слайд 3 start
-changeSlideThree.addEventListener("click", function () {
-  changeOneButton.classList.add("main__card-hidden");
-  changeTwoButton.classList.add("main__card-hidden");
-  changeThreeButton.classList.remove("main__card-hidden");
+changePriceSlideThree.addEventListener("click", function () {
+  changePriceOneButton.classList.add("main__card-hidden");
+  changePriceTwoButton.classList.add("main__card-hidden");
+  changePriceThreeButton.classList.remove("main__card-hidden");
 
-  changeOneButtonAdd.classList.remove("main__card-container-button-special-active");
-  changeOneButtonAddOne.classList.remove("main__card-container-button-special-p-active");
-  changeThreeButtonAdd.classList.remove("main__card-container-button-special");
-  changeThreeButtonAddThree.classList.add("main__card-container-button-special-p-active");
+  changePriceThreeButtonAdd.classList.remove("main__card-container-button-special");
+  changePriceThreeButtonAddThree.classList.add("main__card-container-button-special-p-active");
+  changePriceOneButtonAdd.classList.remove("main__card-container-button-special-active");
+  changePriceOneButtonAddOne.classList.remove("main__card-container-button-special-p-active");
 
-  changeSlideOne.classList.remove("main__card-container-button-active");
-  changeSlideTwo.classList.remove("main__card-container-button-active");
-  changeSlideThree.classList.add("main__card-container-button-active");
+  changePriceSlideOne.classList.remove("main__card-container-button-active");
+  changePriceSlideTwo.classList.remove("main__card-container-button-active");
+  changePriceSlideThree.classList.add("main__card-container-button-active");
 });
 // Открывает слайд 3 end
