196a4d4dbc946fd851009948cdd836fa0ebc65cd
Evgeny <36944054+EvgenyFirst@users.noreply.github.com>
2023-10-25 18:06:33 +0600

    page development 34

--- js/map-popup.js
+++ js/map-popup.js
@@ -5,15 +5,19 @@
 const buttonOpenTheMap = document.querySelector(".main__about-right-map-link");
 var modalMap = document.querySelector(".modal__map");
 var closeModalMap = document.querySelector(".modal__map-button-close");
+var modalIframe = document.querySelector(".modal__iframe");
 
 buttonOpenTheMap.addEventListener("click", function (linkGoDefault) {
-  linkGoDefault.preventDefault(), modalMap.classList.add("modal__map-opened");
+  linkGoDefault.preventDefault(),
+  modalMap.classList.add("modal__map-opened");
+  modalIframe.focus();
 }),
 closeModalMap.addEventListener("click", function (linkGoDefault) {
-  linkGoDefault.preventDefault(), modalMap.classList.remove("modal__map-opened");
+  linkGoDefault.preventDefault(),
+  modalMap.classList.remove("modal__map-opened");
   }),
 window.addEventListener("keydown", function (linkGoDefault) {
-  27 === linkGoDefault.keyCode &&
-    modalMap.classList.contains("modal__map-opened") &&
-    (linkGoDefault.preventDefault(), modalMap.classList.remove("modal__map-opened"));
+27 === linkGoDefault.keyCode &&
+  modalMap.classList.contains("modal__map-opened") &&
+  (linkGoDefault.preventDefault(), modalMap.classList.remove("modal__map-opened"));
 });
